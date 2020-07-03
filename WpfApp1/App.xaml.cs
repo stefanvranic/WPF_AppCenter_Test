e9d31bfb80b8c49890fd0830c9a6b281bb5efd1d
stefan <stefan.vranic996@gmail.com>
2020-07-03 10:44:13 +0200

    Initial Commit

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppCenter.Start("b70a1577-0b4e-4466-940b-70bcd81f40a1",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+    }
+}
